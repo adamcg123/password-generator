e202f227c019c3c50217fb2f6c2b93dd25d6c472
adamcg123 <adam.Gondorchin26@gmail.com>
2021-11-02 14:29:43 -0500

    first prompt done

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,47 @@
 // Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
+
 // Write password to the #password input
-function writePassword() {
+
+
+
+    function writePassword() {
+    var lwrcase = "abcdefghijklmnopqrstuvwxyz";
+    var upprcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "1234567890";
+    var spclchrcter = "!@#$%^&*()_-+=[]{};:,.<>/?`~";
+    var password = "";
+    var avlblcharctrs = "";
+
+
+var pwlength = prompt("please select a character amount for your password between 8 and 128.")
+// does it meet the length requirements?
+if (pwlength >= 8 && pwlength <=128) {
+  console.log(pwlength)
+}    
+else {
+  alert("Your password has to be a min of 8 characters and a max of 128")
+  return "";
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
